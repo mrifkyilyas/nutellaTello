0af9888f38a643eb46c1c73e6c9afc6956c5426f
mrifkyilyas <45786157+mrifkyilyas@users.noreply.github.com>
2019-11-29 10:19:12 +0700

    FIX : isUnique validator

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        transformOptions: { enableImplicitConversion: true },
+        // disableErrorMessages: true,
+        exceptionFactory: (errors: ValidationError[]) =>
+            new BadRequestException(errors),
+    }),
+);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
